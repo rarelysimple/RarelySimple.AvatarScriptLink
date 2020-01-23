0cf0cd3cc7c852bab1e298d9aef04c404e67c42d
Scott Olson <scottolsonjr@users.noreply.github.com>
2020-01-23 08:22:52 -0800

    Remove code to prepare for pages

--- RarelySimple.AvatarScriptLink/Objects/Advanced/Interfaces/IOptionObject2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RarelySimple.AvatarScriptLink.Objects.Advanced
-{
-    public interface IOptionObject2 : IOptionObject
-    {
-        string NamespaceName { get; set; }
-        string ParentNamespace { get; set; }
-        string ServerName { get; set; }
-    }
-}
--- RarelySimple.AvatarScriptLink/Objects/Advanced/Interfaces/IOptionObject2015.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RarelySimple.AvatarScriptLink.Objects.Advanced
-{
-    public interface IOptionObject2015: IOptionObject2
-    {
-        string SessionToken { get; set; }
-    }
-}
